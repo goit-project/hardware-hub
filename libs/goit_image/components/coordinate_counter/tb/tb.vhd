f42347628177b3f9bd4269811dafcfc3b0f9bf90
Rihards Novickis <rihards.novickis@edi.lv>
2024-01-18 18:40:45 +0200

    Added full counter overflow test for coordinate counter

--- libs/goit_image/components/coordinate_counter/tb/tb.vhd
+++ libs/goit_image/components/coordinate_counter/tb/tb.vhd
@@ -2,18 +2,21 @@ library ieee;
 library goit_common;
 library goit_image;
 library vunit_lib;
+library osvvm;
 
 context vunit_lib.vunit_context;
 
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 use vunit_lib.com_pkg.all;
+use osvvm.RandomPkg.all;
 
 
 entity tb is
   generic(
     runner_cfg : string;
-    COUNTER_MAX_VALUE : natural := 15
+    COUNTER_MAX_VALUE_X : natural := 15;
+    COUNTER_MAX_VALUE_Y : natural := 15
   );
 end entity;
 
@@ -58,6 +61,12 @@ begin
   -- VUnit test sequencer
   -----------------------------------------------------------------------------
   process
+    -- dut/simulation variables
+    variable random : RandomPType;
+    variable i, dut_y, dut_x : integer := 0;
+    variable dut_counter_max_y, dut_counter_max_x : integer := 0;
+
+
     procedure dut_cycle_reset is
     begin
       arst_n <= '0';
@@ -65,21 +74,45 @@ begin
       arst_n <= '1';
     end procedure;
 
-    --variable expected : integer;
+
+    procedure dut_configure(max_x, max_y : in natural) is
+    begin
+      i_counter_max_x   <= to_unsigned(max_x, i_counter_max_x'length);
+      i_counter_max_y   <= to_unsigned(max_y, i_counter_max_y'length);
+      dut_counter_max_x := max_x;
+      dut_counter_max_y := max_y;
+    end procedure;
+
+
+    procedure dut_model_update is
+    begin
+      if en = '1' then
+        if dut_x < dut_counter_max_x then
+          dut_x := dut_x + 1;
+        else
+          dut_x := 0;
+          if dut_y < dut_counter_max_y then
+            dut_y := dut_y + 1;
+          else
+            dut_y := 0;
+          end if;
+        end if;
+      end if;
+    end procedure;
   begin
     test_runner_setup(runner, runner_cfg);
+    random.InitSeed(random'instance_name);
 
     while test_suite loop
 
       if run("reset") then
         -- prepare counter
-        i_counter_max_x <= to_unsigned(COUNTER_MAX_VALUE, i_counter_max_x'length);
-        i_counter_max_y <= to_unsigned(COUNTER_MAX_VALUE, i_counter_max_y'length);
+        dut_configure(COUNTER_MAX_VALUE_X, COUNTER_MAX_VALUE_Y);
         dut_cycle_reset;
         en <= '1';
 
         -- wait couple of counter iterations
-		for i in 0 to COUNTER_MAX_VALUE+3 loop
+        for i in 0 to COUNTER_MAX_VALUE_X+3 loop
           wait until rising_edge(clk);
         end loop;
 
@@ -94,11 +127,39 @@ begin
 
 
       elsif run("counter_full") then
-	    error("Test not implemented");
+        -- prepare counter
+        dut_configure(COUNTER_MAX_VALUE_X, COUNTER_MAX_VALUE_Y);
+        dut_cycle_reset;
+        en <= '1';
+
+        -- simulate two full cycles of X and Y counter overflow
+        while (i < 2) loop
+          en <= random.RandSl;
+		  wait for CLK_PERIOD*0.1;
+
+          -- compare DUT and model
+          check(o_counter_y = dut_y, "Y counter value check ("
+            & "EXPECTED: " & integer'image(dut_y) & "; "
+            & "GOT: " & integer'image(to_integer(o_counter_y))
+            & ")");
+          check(o_counter_x = dut_x, "X counter value check ("
+            & "EXPECTED: " & integer'image(dut_x) & "; "
+            & "GOT: " & integer'image(to_integer(o_counter_x))
+            & ")");
+
+          -- update DUT for the next iteration
+          dut_model_update;
+
+          -- next clock cycle + simulation finalization check
+          wait until falling_edge(clk);
+          if dut_x = dut_counter_max_x and dut_y = dut_counter_max_y then
+             i := i + 1;
+          end if;
+        end loop;
 
 
       elsif run("counter_full_variable") then
-	    error("Test not implemented");
+        error("Test not implemented");
 
       end if;
 
