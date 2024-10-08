de2774e1749700b503dbd47c2041d984000babd2
Rihards Novickis <rihards.novickis@edi.lv>
2024-10-08 10:18:27 +0300

    Temporary change in manifest file, due to issues with hdlmake finding goit package

--- Manifest.py
+++ Manifest.py
@@ -1,6 +1,17 @@
-import goit.dependencies
+import glob
+#import os
+#import goit.dependencies
+
+# aggregate libraries
+#dir_path = os.path.dirname(os.path.realpath(__file__))
+libs = glob.glob('libs/*')
+#libs = glob.glob(dir_path + '/libs/*')
 
 # Aggregate library hierarchy
 modules = {
-  "local" : goit.dependencies.get_library_paths()
+  #"local" : ["/home/rihards/projects/codebase/goit_hardware_hub/libs/goit_common/"]
+  #"local" : ["libs/goit_common"]
+  #"local" : libs
+  "local" : [lib for lib in libs]
+  #"local" : goit.dependencies.get_library_paths()
 }
--- libs/goit_image/Manifest.py
+++ libs/goit_image/Manifest.py
@@ -1,9 +1,10 @@
-import goit.dependencies
+import glob
+#import goit.dependencies
 
 # Module defines library
 library = "goit_image"
 
 # Array with design unit filenames 
-files = goit.dependencies.get_component_paths() \
-      + goit.dependencies.get_procedure_paths()
-
+files = glob.glob('pkg/src/*.vhd') + glob.glob('components/*/src/*.vhd')
+#files = goit.dependencies.get_component_paths() \
+#      + goit.dependencies.get_procedure_paths()
